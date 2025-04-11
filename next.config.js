98c9c67ead9145d502e89e9ef460bf7512025a66
Ram Kardile <ramkardile4ever@gmail.com>
2025-04-11 07:35:44 +0530

    first deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   images: {
     domains: ['nsohhtlgbafrydzjkqle.supabase.co'],
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+    return config
+  },
+  experimental: {
+    esmExternals: 'loose'
   }
 }
 
